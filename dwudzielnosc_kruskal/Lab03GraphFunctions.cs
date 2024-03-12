3d283b8aabc7e25a984ae6654db5ef723efe9448
jakubkindracki <01180792@pw.edu.pl>
2024-03-12 18:07:02 +0100

    Initial commit

---
+++ dwudzielnosc_kruskal/Lab03GraphFunctions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using ASD.Graphs;
+using ASD;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+    public class Lab03GraphFunctions : System.MarshalByRefObject
+    {
+
+        // Część 1
+        // Wyznaczanie odwrotności grafu
+        //   0.5 pkt
+        // Odwrotność grafu to graf skierowany o wszystkich krawędziach przeciwnie skierowanych niż w grafie pierwotnym
+        // Parametry:
+        //   g - graf wejściowy
+        // Wynik:
+        //   odwrotność grafu
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Graf wynikowy musi być w takiej samej reprezentacji jak wejściowy
+        public DiGraph Lab03Reverse(DiGraph g)
+        {
+            DiGraph gRes = new DiGraph(g.VertexCount, g.Representation);
+            foreach (var edge in g.DFS().SearchAll())
+                gRes.AddEdge(edge.To, edge.From);
+
+            return gRes;
+        }
+
+        // Część 2
+        // Badanie czy graf jest dwudzielny
+        //   0.5 pkt
+        // Graf dwudzielny to graf nieskierowany, którego wierzchołki można podzielić na dwa rozłączne zbiory
+        // takie, że dla każdej krawędzi jej końce należą do róźnych zbiorów
+        // Parametry:
+        //   g - badany graf
+        //   vert - tablica opisująca podział zbioru wierzchołków na podzbiory w następujący sposób
+        //          vert[i] == 1 oznacza, że wierzchołek i należy do pierwszego podzbioru
+        //          vert[i] == 2 oznacza, że wierzchołek i należy do drugiego podzbioru
+        // Wynik:
+        //   true jeśli graf jest dwudzielny, false jeśli graf nie jest dwudzielny (w tym przypadku parametr vert ma mieć wartość null)
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Podział wierzchołków może nie być jednoznaczny - znaleźć dowolny
+        //   3) Pamiętać, że każdy z wierzchołków musi być przyporządkowany do któregoś ze zbiorów
+        //   4) Metoda ma mieć taki sam rząd złożoności jak zwykłe przeszukiwanie (za większą będą kary!)
+        public bool Lab03IsBipartite(Graph g, out int[] vert)
+        {
+            vert = new int[g.VertexCount];
+            foreach (var edge in g.DFS().SearchAll())
+            {
+                if (vert[edge.From] == 0 && vert[edge.To] == 0)
+                {
+                    vert[edge.From] = 1;
+                    vert[edge.To] = 2;
+                }
+                else if (vert[edge.From] == 0)
+                    vert[edge.From] = 3 - vert[edge.To];
+                else if (vert[edge.To] == 0)
+                    vert[edge.To] = 3 - vert[edge.From];
+                else if (vert[edge.From] == vert[edge.To])
+                {
+                    vert = null;
+                    return false;
+                }
+            }
+
+            for(int i = 0; i < g.VertexCount; i++)
+                if (vert[i] == 0)
+                    vert[i] = 1;
+
+            return true;
+        }
+
+        // Część 3
+        // Wyznaczanie minimalnego drzewa rozpinającego algorytmem Kruskala
+        //   1 pkt
+        // Schemat algorytmu Kruskala
+        //   1) wrzucić wszystkie krawędzie do "wspólnego worka"
+        //   2) wyciągać z "worka" krawędzie w kolejności wzrastających wag
+        //      - jeśli krawędź można dodać do drzewa to dodawać, jeśli nie można to ignorować
+        //      - punkt 2 powtarzać aż do skonstruowania drzewa (lub wyczerpania krawędzi)
+        // Parametry:
+        //   g - graf wejściowy
+        //   mstw - waga skonstruowanego drzewa (lasu)
+        // Wynik:
+        //   skonstruowane minimalne drzewo rozpinające (albo las)
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Wykorzystać klasę UnionFind z biblioteki Graph
+        //   3) Jeśli graf g jest niespójny to metoda wyznacza las rozpinający
+        //   4) Graf wynikowy (drzewo) musi być w takiej samej reprezentacji jak wejściowy
+        public Graph<int> Lab03Kruskal(Graph<int> g, out int mstw)
+        {
+            var edges = new PriorityQueue<int, Edge<int>>();
+            foreach (var edge in g.DFS().SearchAll())
+                edges.Insert(edge, edge.Weight);
+            Graph<int> res = new Graph<int>(g.VertexCount, g.Representation);
+            UnionFind uf = new UnionFind(g.VertexCount);
+            mstw = 0;
+            while (edges.Count > 0)
+            {
+                Edge<int> e = edges.Extract();
+                int aSet = uf.Find(e.From);
+                int bSet = uf.Find(e.To);
+                if (aSet != bSet)
+                {
+                    uf.Union(aSet, bSet);
+                    res.AddEdge(e.From, e.To, e.Weight);
+                    mstw += e.Weight;
+                    if (res.EdgeCount >= res.VertexCount - 1)
+                        break;
+                }
+            }
+            
+            return res;
+        }
+
+        // Część 4
+        // Badanie czy graf nieskierowany jest acykliczny
+        //   0.5 pkt
+        // Parametry:
+        //   g - badany graf
+        // Wynik:
+        //   true jeśli graf jest acykliczny, false jeśli graf nie jest acykliczny
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Najpierw pomysleć jaki, prosty do sprawdzenia, warunek spełnia acykliczny graf nieskierowany
+        //      Zakodowanie tego sprawdzenia nie powinno zająć więcej niż kilka linii!
+        //      Zadanie jest bardzo łatwe (jeśli wydaje się trudne - poszukać prostszego sposobu, a nie walczyć z trudnym!)
+        public bool Lab03IsUndirectedAcyclic(Graph g)
+        {
+            UnionFind forest = new UnionFind(g.VertexCount);
+            Graph checkEdges = new Graph(g.VertexCount, g.Representation);
+            foreach (var edge in g.DFS().SearchAll())
+            {
+                if (checkEdges.HasEdge(edge.From, edge.To))
+                    continue;
+                checkEdges.AddEdge(edge.From, edge.To);
+                int aSet = forest.Find(edge.From);
+                int bSet = forest.Find(edge.To);
+                if (aSet != bSet)
+                    forest.Union(aSet, bSet);
+                else
+                    return false;
+            }
+            
+            return true;
+        }
+
+    }
+
+}
---
+++ silne_kolorowanie_krawedzi/StrongEdgeColoring.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ASD
+{
+    using ASD.Graphs;
+    using System.Collections.Generic;
+
+    public class Lab03 : System.MarshalByRefObject
+    {
+        // Część I
+        // Funkcja zwracajaca kwadrat danego grafu.
+        // Kwadratem grafu nazywamy graf o takim samym zbiorze wierzchołków jak graf pierwotny, w którym wierzchołki
+        // połączone sa krawędzią jeśli w grafie pierwotnym były polączone krawędzia bądź ścieżką złożoną z 2 krawędzi
+        // (ale pętli, czyli krawędzi o początku i końcu w tym samym wierzchołku, nie dodajemy!).
+        public Graph Square(Graph graph)
+        {
+            // // Dziala ale wolno
+            // Graph square = new Graph(graph.VertexCount, graph.Representation);
+            // foreach (var edge in graph.DFS().SearchAll())
+            // {
+            //     square.AddEdge(edge.To, edge.From);
+            //     foreach (var secondEdge in graph.DFS().SearchAll())
+            //     {
+            //         if (edge.From == secondEdge.To && secondEdge.From != edge.To && 
+            //             !square.HasEdge(secondEdge.From, edge.To))
+            //             square.AddEdge(secondEdge.From, edge.To);
+            //         if (edge.To == secondEdge.From && secondEdge.To != edge.From &&
+            //             !square.HasEdge(secondEdge.To, edge.From))
+            //             square.AddEdge(secondEdge.To, edge.From);
+            //         if (edge.To == secondEdge.To && edge.From != secondEdge.From && 
+            //             !square.HasEdge(edge.From, secondEdge.From))
+            //             square.AddEdge(edge.From, secondEdge.From);
+            //     }
+            // }
+            
+            // DUZO SZYBIEJ !!!
+            // Teraz sposob po wierzcholkach
+            Graph square = new Graph(graph.VertexCount, graph.Representation);
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                foreach (var n in graph.OutNeighbors(i))
+                {
+                    foreach (var n2 in graph.OutNeighbors(n))
+                    {
+                        if (n2 != i && !square.HasEdge(i, n2))
+                            square.AddEdge(i, n2);
+                    }
+
+                    if (!square.HasEdge(i, n))
+                        square.AddEdge(i, n);
+                }
+            }
+            
+            return square;
+        }
+
+        // Część II
+        // Funkcja zwracająca Graf krawędziowy danego grafu.
+        // Wierzchołki grafu krawędziwego odpowiadają krawędziom grafu pierwotnego, wierzcholki grafu krawędziwego
+        // połączone sa krawędzią jeśli w grafie pierwotnym z krawędzi odpowiadającej pierwszemu z nich można przejść
+        // na krawędź odpowiadającą drugiemu z nich przez wspólny wierzchołek.
+
+        // Tablicę names tworzymy i wypełniamy według następującej zasady.
+        // Każdemu wierzchołkowi grafu krawędziowego odpowiada element tablicy names (o indeksie równym numerowi wierzchołka)
+        // zawierający informację z jakiej krawędzi grafu pierwotnego wierzchołek ten powstał.
+        // Np.dla wierzchołka powstałego z krawedzi <0,1> do tablicy zapisujemy krotke (0, 1) - przyda się w dalszych etapach
+        public Graph LineGraph(Graph graph, out (int x, int y)[] names)
+        {
+            names = null;
+            return null;
+        }
+
+        // Część III
+        // Funkcja znajdujaca poprawne kolorowanie wierzchołków danego grafu nieskierowanego.
+        // Kolorowanie wierzchołków jest poprawne, gdy każde dwa sąsiadujące wierzchołki mają różne kolory
+        // Funkcja ma szukać kolorowania według następujacego algorytmu zachłannego:
+
+        // Dla wszystkich wierzchołków v (od 0 do n-1)
+        // pokoloruj wierzcholek v kolorem o najmniejszym możliwym numerze(czyli takim, na który nie są pomalowani jego sąsiedzi)
+        // Kolory numerujemy począwszy od 0.
+
+        // UWAGA: Podany opis wyznacza kolorowanie jednoznacznie, jakiekolwiek inne kolorowanie, nawet jeśli spełnia formalnie
+        // definicję kolorowania poprawnego, na potrzeby tego zadania będzie uznane za błędne.
+
+        // Funkcja zwraca liczbę użytych kolorów (czyli najwyższy numer użytego koloru + 1),
+        // a w tablicy colors zapamiętuje kolory poszczególnych wierzchołkow.
+        public int VertexColoring(Graph graph, out int[] colors)
+        {
+            colors = null;
+            return 0;
+        }
+
+        // Funkcja znajduje silne kolorowanie krawędzi danego grafu.
+        // Silne kolorowanie krawędzi grafu jest poprawne gdy każde dwie krawędzie, które są ze sobą sąsiednie
+        // (czyli można przejść z jednej na drugą przez wspólny wierzchołek)
+        // albo są połączone inną krawędzią(czyli można przejść z jednej na drugą przez ową inną krawędź), mają różne kolory.
+
+        // Należy zwrocić nowy graf, który będzie miał strukturę identyczną jak zadany graf,
+        // ale w wagach krawędzi zostaną zapisane przydzielone kolory.
+
+        // Wskazówka - to bardzo proste.Należy wykorzystać wszystkie poprzednie funkcje.
+        // Zastanowić się co możemy powiedzieć o kolorowaniu wierzchołków kwadratu grafu krawędziowego?
+        // Jak się to ma do silnego kolorowania krawędzi grafu pierwotnego?
+        public int StrongEdgeColoring(Graph graph, out Graph<int> coloredGraph)
+        {
+            coloredGraph = null;
+            return 0;
+        }
+
+    }
+
+}
